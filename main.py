911f9a57146cbc6aa59197b333f94d928a4af30a
Atish Nishant <nishant.dev2023@gmail.com>
2025-06-08 10:16:48 +0530

    v1.0.0. Complete and Edit

--- main.py
+++ main.py
@@ -8,19 +8,42 @@ while True:
         new_todo=input("Enter a new todo Item :")
         todo_list.append(new_todo)
         print("New todo item has been added")
+
+
     elif user_action=="show" or user_action=='2':
         for index,item in enumerate(todo_list):
             print(f"{index+1}-{item}")
+
+
     elif user_action=="edit" or user_action=='3':
-        print("For show operation")
+        try:
+            todo_index=int(input("Enter the index no of the todo Item:"))
+            new_todo=input("Enter the new label:")
+            todo_list[todo_index-1]=new_todo
+            print("1 todo modified")
+        except Exception as err:
+            print("Exception",err)
+
+
     elif user_action=="complete" or user_action=='4':
-        print("For complete operation")
+        try:
+            todo_index = int(input("Enter the index no of the todo Item:"))
+            complete_todo=todo_list.pop(todo_index-1)
+            print(f"{complete_todo} has been completed and removed from todo list...")
+        except Exception as err:
+            print("Exception", err)
+
+
     elif user_action=="exit" or user_action=='5':
         print("For exit operation")
         break
+
+
     else:
         print("Invalid Entry ,try again")
         continue
+
+
 print("Closing the application")
 
 
